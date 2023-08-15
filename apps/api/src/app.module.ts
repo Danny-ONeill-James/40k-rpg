001b6bbf6d99291b4256d0aac9cbf1c059262382
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-08-15 11:29:31 +0100

    feat: basic Typeorm connection

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CharacterModule } from './imperium-maledictum/character/character.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CharacterModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: 5432,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CharacterModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
