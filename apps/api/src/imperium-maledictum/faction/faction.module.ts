c8b592bbe523d3845c988f0235a1e1b24a217248
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-08-16 10:25:46 +0100

    feat: created mo, s,co

--- apps/api/src/imperium-maledictum/faction/faction.module.ts
+++ apps/api/src/imperium-maledictum/faction/faction.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FactionEntity } from './entities/faction.entity';
+import { FactionService } from './faction.service';
+import { FactionController } from './faction.controller';
 
-@Module({ imports: [TypeOrmModule.forFeature([FactionEntity])] })
+@Module({ imports: [TypeOrmModule.forFeature([FactionEntity])], providers: [FactionService], controllers: [FactionController] })
 export class FactionModule {}
