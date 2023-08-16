0c49cf9591c06988e533f075484e8cb71a9a7683
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-08-16 15:37:39 +0100

    Feat: fixed Entity and added basic creation script

--- apps/api/src/imperium-maledictum/role/role.controller.ts
+++ apps/api/src/imperium-maledictum/role/role.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateRoleDto } from './dtos/role.dto';
+import { IRole } from './interfaces/role.interface';
+import { RoleService } from './role.service';
 
 @Controller('role')
-export class RoleController {}
+export class RoleController {
+  constructor(private roleService: RoleService) {}
+
+  @Post()
+  CreateNew(@Body() roleDetails: CreateRoleDto[]): Promise<IRole[]> {
+    return this.roleService.createNew(roleDetails);
+  }
+}
--- apps/api/src/imperium-maledictum/role/role.service.ts
+++ apps/api/src/imperium-maledictum/role/role.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateRoleDto } from './dtos/role.dto';
+import { RoleEntity } from './entities/role.entity';
+import { IRole } from './interfaces/role.interface';
 
 @Injectable()
-export class RoleService {}
+export class RoleService {
+  constructor(
+    @InjectRepository(RoleEntity)
+    private roleRepository: Repository<RoleEntity>,
+  ) {}
+
+  async createNew(newRoles: CreateRoleDto[]): Promise<IRole[]> {
+    newRoles.forEach(async (newRole) => {
+      if (
+        !(await this.roleRepository.exist({
+          where: { name: newRole.name },
+        }))
+      ) {
+        await this.roleRepository.save(newRole);
+      }
+    });
+
+    return newRoles;
+  }
+}
